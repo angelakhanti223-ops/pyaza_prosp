bacb8b443fbdb28bc0337880f0ef2da36fc412fe
angelakhanti223-ops <angelakhanti223@gmail.com>
2026-09-21 10:25:11 +0300

    Update MAX link in article CTA

--- frontend/src/components/blog/ArticleLeadButtons.tsx
+++ frontend/src/components/blog/ArticleLeadButtons.tsx
@@ -3,7 +3,7 @@
 import { ClipboardList, MessageCircle, Send } from "lucide-react";
 import OpenLeadFormButton from "@/components/lead-form/OpenLeadFormButton";
 
-const MAX_LINK = "https://max.ru/sletat_ru_pnz";
+const MAX_LINK = "https://max.ru/u/f9LHodD0cOKXx27XgipyAzI5okKvOG6U1UMUKwBiAs7TOdQPGEkqcG7cLH8";
 const TELEGRAM_LINK = "https://t.me/sletat_ru_pnz";
 
 type Props = {
